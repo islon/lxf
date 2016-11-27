c354bfb4d301b627bc3154197da052f40854e504
islon <longai1567@163.com>
2016-11-28 02:44:24 +0800

    add 20161128

--- image2.py
+++ image2.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import Image
+from PIL import Image
 import os
+import ImageFilter
 
 def foo(fullpath):
 	print "******Start******"
@@ -9,10 +10,16 @@ def foo(fullpath):
 		if os.path.isfile(childpath):
 			childimage=Image.open(childpath)
 			w,h=childimage.size
+			print u"改变尺寸"
 			childimage.thumbnail((w//2, h//2))
+			print u"保存到新的路径"
 			newchildpath=os.path.join(fullpath,"new",os.path.split(childpath)[1])
 			print os.path.split(childpath)[1]
+			print u"模糊图片"
+			childimage=childimage.filter(ImageFilter.BLUR)
+			print u"保存图片"
 			childimage.save(newchildpath,'jpeg')
+			print u"保存成功"
 	print "******Over******"
 if __name__ =="__main__":
-	foo('I:\\python\\pic\\more')
+	foo('D:\\python\\pic\\more')
--- open.py
+++ open.py
@@ -1,7 +1,37 @@
+# -*- coding= utf-8 -*-
+
+#打开文档、读取内容
 try:
-	f=open('I:/python/txt/hello.txt','r')
+	f=open('D:\\python\\txt\\hello.txt','r')
 	print f.read()
 finally:
 	if f:
-		f.close()
-		print "error"
+		f.close
+		print '--------f is close--------'
+
+#打开文档、读取内容、每行显示文字，精简版
+with open('D:\\python\\txt\\hello.txt','r') as f:
+	for line in f.readlines():
+		print '----',line.strip()
+if f:
+	f.close
+	print '--------f is close--------'
+	
+
+#打开二进制：图片、视频等
+#with open('D:\\python\\pic\\girl_1.jpg','rb') as f:
+#	print f.read()
+	#显示出来的是16进制的字节
+
+#写文件
+with open('D:\\python\\txt\\write.txt','a') as f:
+	f.write('this is first line')
+	print u'--------写入成功--------'.encode("GBk")
+with open('D:\\python\\txt\\write.txt','r') as r:
+	print r.read()
+	
+with open('D:\\python\\txt\\write.txt','a') as f:
+	f.write('\nthis is second line')
+	print u'--------写入成功--------'.encode("GBk")
+with open('D:\\python\\txt\\write.txt','r') as r:
+	print r.read()
